6742fe47cc61b2d63b167103c442f741007b2645
cryptedveen <59870131+cryptedveen@users.noreply.github.com>
2020-01-24 15:36:26 +0530

    Add files via upload

---
+++ precache-manifest.f42c91eb86b42e00dfec706ed9346c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805624ee5ead84c91b0061999be14abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "794c8a95fd229dcd3f61",
+    "url": "/static/css/main.0eba015a.chunk.css"
+  },
+  {
+    "revision": "e4a4ce0fb6e4d95bca3f",
+    "url": "/static/js/2.fa0581a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fa0581a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "794c8a95fd229dcd3f61",
+    "url": "/static/js/main.48137ad0.chunk.js"
+  },
+  {
+    "revision": "751a94f3925490dc4868",
+    "url": "/static/js/runtime-main.21e0ae3d.js"
+  }
+]);
